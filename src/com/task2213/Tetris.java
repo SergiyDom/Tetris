17845c67fa0a71ec70f6b867f50205c906817464
SergiyDom <sergiy.domaranskiy@gmail.com>
2022-05-29 20:32:37 +0300

    added essentials methods and fields

--- src/com/task2213/Tetris.java
+++ src/com/task2213/Tetris.java
@@ -1,7 +1,31 @@
 package com.task2213;
 
 public class Tetris {
-    public static void main(String[] args) {
+    private Field field;
+    private Figure figure;
 
+    public static Tetris game;
+
+    public static void main(String[] args) {
+        Tetris tetris = new Tetris();
+        game = tetris;
+        game.run();
+
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void run() {
+
+    }
+
+    public void step() {
+        
     }
 }
