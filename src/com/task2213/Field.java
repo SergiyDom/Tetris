326ec08c4674e6d7ab2bd7225de51d5fc507ada8
SergiyDom <sergiy.domaranskiy@gmail.com>
2022-05-29 20:47:31 +0300

    created the backbone of the class Field

--- src/com/task2213/Field.java
+++ src/com/task2213/Field.java
@@ -1,4 +1,42 @@
 package com.task2213;
 
-public class Field {
+public class Field { //Он будет отвечать за хранение данных о текущих занятых и свободных клетках на поле игры.
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new int[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void print() { //объект будет отрисовывать на экран свое текущее состояние;
+
+    }
+
+    public void removeFullLines() { //будет удалять из матрицы полностью заполненные строки и сдвигать вышележащие строки вниз;
+
+    }
+
+    public Integer getValue(int x, int y) { //возвращает значение которое находится в матрице с координатами x и y;
+
+        return null;
+    }
+
+    public void setValue(int x, int y, int value) {
+        matrix[x][y] = value;
+    }
 }
